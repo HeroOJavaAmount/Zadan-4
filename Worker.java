cc6e4a71624a4c3b2ad4e8e5609cffd645155ceb
Alexandr_O <aleksandr.osha@yahoo.com>
2026-01-18 20:30:30 +0300

    Add files via upload

---
+++ Worker.java
@@ -0,0 +1,23 @@
+public class Worker implements OnTaskDoneListener {
+    private OnTaskDoneListener callback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+
+    @Override
+    public void onDone(String result) {
+
+    }
+}
+
+@FunctionalInterface
+interface OnTaskDoneListener {
+    void onDone(String result);
+}
